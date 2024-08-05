788371a1e54fdfd4c514770e724bc7f1ee733913
Olga Pavlova <olgapavlova@gmail.com>
2024-08-05 18:56:24 +0300

    todo without sh and for any folder

--- src/todo.sh
+++ src/todo.sh
@@ -1,7 +1,6 @@
-#!/usr/bin/bash
+#!/bin/bash
 clear
 TODO_FILE=.todo
-cd ..
 rm -f $TODO_FILE
 grep -RHn TODO --exclude-dir=tools --exclude=.* > $TODO_FILE
 sed -i -r 's/([^\:]*):([^\:]*).*TODO(.*)/\1\t[\2] \3/' $TODO_FILE
