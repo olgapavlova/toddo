0b49b8ffd5c6cf18c2f7e73aa897dfde3b60def1
Olga Pavlova <olgapavlova@gmail.com>
2024-08-06 22:26:24 +0300

    todo improvements

--- src/todo.sh
+++ src/todo.sh
@@ -2,6 +2,7 @@
 clear
 TODO_FILE=.todo
 rm -f $TODO_FILE
-grep -RHn TODO --exclude-dir=tools --exclude=.* > $TODO_FILE
-sed -i -r 's/([^\:]*):([^\:]*).*TODO(.*)/\1\t[\2] \3/' $TODO_FILE
-more $TODO_FILE
+grep -RHn TODO --exclude-dir .venv --exclude-dir .git > $TODO_FILE
+sed -r 's/([^\:]*):([^\:]*).*TODO(.*)/\1\t[\2] \3/' $TODO_FILE
+sed '!d;=' $TODO_FILE | paste -d: - -
+//more $TODO_FILE
