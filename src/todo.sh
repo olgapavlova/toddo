424ac18284310f763e2fbb76ceac78bb93cdd375
Olga Pavlova <olgapavlova@gmail.com>
2024-08-12 16:34:29 +0300

    todo NUM feature

--- src/todo.sh
+++ src/todo.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-clear
 TODO_FILE=.todo
+
+if [ -n "$1" ]
+then
+
+# Open todo in vim by line
+VIM_PARAM=$(sed -r -n "$1s/.*\[([0-9]*)\].\.\/(.*)/\2 +\1/p" $TODO_FILE)
+vim $VIM_PARAM
+
+
+else
+
+# Generate .todo file from scratch
+clear
 rm -f $TODO_FILE
-grep -RHn TODO --exclude-dir .venv --exclude-dir .git > $TODO_FILE
-sed -r 's/([^\:]*):([^\:]*).*TODO(.*)/\1\t[\2] \3/' $TODO_FILE
-sed '!d;=' $TODO_FILE | paste -d: - -
-//more $TODO_FILE
+grep -RHn TODO --exclude-dir .venv --exclude-dir .git | sed -r 's/([^\:]*):([^\:]*).*TODO (.*)/\3\t[\2] \1/' > $TODO_FILE
+more -S -N -E $TODO_FILE
+
+fi
